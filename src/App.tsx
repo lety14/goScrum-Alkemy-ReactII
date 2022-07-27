faf210e2ac1ff56792f5e8a61e1cde9067bff6e3
Leti <leticiapistarelli@gmail.com>
2022-07-26 22:18:44 -0300

    refactor App.tsx and other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import { Error404 } from "./pages/PageNotFound/Error404.page";
+import { Login } from "./pages/Auth/Login";
+import { Register } from "./pages/Auth/Register";
+import Tasks from "./pages/Task/Tasks.page";
+import { GlobalStyle } from "./App.style";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+import ThemeProvider, { ThemeContext } from "./context/themeProvider.context";
+import { useContext } from "react";
+
 export const App = () => {
-  return <div className="App">GO SCRUM</div>;
+  const { theme } = useContext(ThemeContext);
+
+  return (
+    <Provider store={store}>
+      <div className={theme === "light" ? "appLight" : "appDark"}>
+        {/* <AnimatePresence> */}
+        <Routes>
+          <Route path="/" element={<Tasks />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+        {/* </AnimatePresence> */}
+      </div>
+    </Provider>
+  );
 };
